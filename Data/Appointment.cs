85c068ffc1a295adc587ca36363d22df5da4e8f7
Ryan <ryansheikh003@gmail.com>
2023-12-07 11:11:34 -0700

    Appointment page completed

---
+++ Data/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalSystem.Data
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public Appointment(int i) 
+        {
+            Id = i;
+        }
+    }
+}
